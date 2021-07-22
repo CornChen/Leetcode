34e9bded0ed6b07776e933f8d1bc9bffd9430fc3
chenyukun <13006322095@163.com>
2021-07-22 14:38:07 +0800

    feat(medium):17

--- src/main/java/medium/Lc17.java
+++ src/main/java/medium/Lc17.java
@@ -1,7 +1,6 @@
 package medium;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /** 电话号码的字母组合
@@ -23,14 +22,38 @@ import java.util.List;
  提示：
  0 <= digits.length <= 4
  digits[i] 是范围 ['2', '9'] 的一个数字。
+ https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/solution/tong-su-yi-dong-dong-hua-yan-shi-17-dian-hua-hao-m/
  */
 public class Lc17 {
+//    char[] digit = {'2','3','4','5','6','7','8','9'};
+//    char[][] c = {{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'},{'p','q','r','s'},{'t','u','v'},{'w','x','y','z'}};
+    public static void main(String[] args) {
+        System.out.println(letterCombinations("23"));
+    }
 
-    char[] digit = {'2','3','4','5','6','7','8','9'};
-    char[][] c = {{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'},{'p','q','r','s'},{'t','u','v'},{'w','x','y','z'}};
+    static String[] letter = {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    static List<String> res = new ArrayList<>();
 
-    public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<>();
-        char[] chars = digits.toCharArray();
+    public static List<String> letterCombinations(String digits) {
+        if(digits == null || digits.length() == 0) {
+            return new ArrayList<>();
+        }
+        iterStr(digits, new StringBuilder(), 0);
+        return res;
+    }
+
+    static void iterStr(String digits, StringBuilder sb, int index){
+        if(index == digits.length()){
+            res.add(sb.toString());
+            return;
+        }
+        char c = digits.charAt(index);
+        int pos = c - '2';
+        String map_string = letter[pos];
+        for(int i = 0; i < map_string.length(); i++){
+            sb.append(map_string.charAt(i));
+            iterStr(digits, sb, index + 1);
+            sb.deleteCharAt(sb.length() - 1);
+        }
     }
 }
